0dbc0d590975379d1a26429a7c3f7fa666054864
rkumar9090 <rkumar9090@hotmail.com>
2020-01-26 20:53:15 +0530

    SwitchCase Example

---
+++ BegineersBug/src/com/geeks/overloading/SwitchCaseExample.java
@@ -0,0 +1,27 @@
+package com.geeks.overloading;
+
+public class SwitchCaseExample {
+
+	public static void main(String[] args) {
+		try {
+			String language = "python";
+
+			switch (language) {
+			case "JAVA":
+				System.out.println("Java is choosed");
+				break;
+			case "C++":
+				System.out.println("C++ is choosed");
+			case "python":
+				System.out.println("Python is choosed");
+				break;
+			default:
+				System.out.println("default");
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
