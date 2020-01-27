8f83a1373bf21caf47b3b6a187150f02e8165164
rkumar9090 <rkumar9090@hotmail.com>
2020-01-27 10:09:24 +0530

    Ternary Operator

---
+++ BegineersBug/src/com/geeks/overloading/TernaryOperatorExample.java
@@ -0,0 +1,9 @@
+package com.geeks.overloading;
+
+public class TernaryOperatorExample {
+
+	public static void main(String[] args) {
+		String s = null;
+		System.out.println((s == null) ? "String s is null" : "String s is not null");
+	}
+}
