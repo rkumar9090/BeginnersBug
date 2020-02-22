b35117d8b64f37de0ecce1970e59d35e011b4dcd
rkumar9090 <rkumar9090@hotmail.com>
2020-02-22 14:03:53 +0530

    ConvertInttoString

---
+++ BegineersBug/src/com/geeks/example/ConvertInttoString.java
@@ -0,0 +1,12 @@
+package com.geeks.example;
+
+public class ConvertInttoString {
+
+	public static void main(String[] args) {
+
+		int i = 99;
+		String stringValue = String.valueOf(i);
+		System.out.println("Converted String " + stringValue);
+	}
+
+}
