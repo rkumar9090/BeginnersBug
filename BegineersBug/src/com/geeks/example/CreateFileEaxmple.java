def4905158754cd6bba1f5179d254693b68cea73
rkumar9090 <rkumar9090@hotmail.com>
2020-03-03 09:19:11 +0530

    Create file using java BeginnersBug

---
+++ BegineersBug/src/com/geeks/example/CreateFileEaxmple.java
@@ -0,0 +1,25 @@
+package com.geeks.example;
+
+import java.io.File;
+
+public class CreateFileEaxmple {
+
+	public static void main(String[] args) {
+		try {
+			File file = new File("C://temp//sample.txt");
+			// .exists Method check if file already exists on the path
+			if (!file.exists()) {
+				// Below line will create file sample.txt on C://temp// folder
+				file.createNewFile();
+				System.out.println("File created succesfully !!");
+			} else {
+
+				System.err.println("File already exists");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
